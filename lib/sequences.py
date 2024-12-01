62a1bf5bb834f402f3d020bd9464a4ab5d6f58d9
Andrew Karanja Njiyo <karanjaandrew2000@outlook.com>
2024-12-01 13:55:36 +0300

    fibonacci sequence

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print([])
+    elif length == 1:
+        print ([0])
+    elif length == 2:
+        print([0,1])
+    else:
+        fibonacci = [0,1]
+        while len(fibonacci) < length:
+            next_num = fibonacci[-1] + fibonacci[-2]
+            fibonacci.append(next_num)
+        
+        print(fibonacci)
